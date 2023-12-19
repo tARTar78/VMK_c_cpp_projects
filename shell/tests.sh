81a2c3c90e0284301cd0d239396fe0bd5a43c9ae
tARTar78 <143780971+tARTar78@users.noreply.github.com>
2023-12-19 23:12:51 +0300

    Update tests.sh

--- shell/tests.sh
+++ shell/tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Running test 1..."
-./shell_art < test1_input.txt > test1_output.txt
-if diff -q test1_expected_output.txt test1_output.txt
+./shell_art < /tmp/test0_input.txt > test0_output.txt
+if diff -q /tmp/test0_expected_output.txt test0_output.txt
 then
     echo "Test 1 passed"
 else
@@ -10,8 +10,8 @@ else
 fi
 
 echo "Running test 2..."
-./shell_art < test2_input.txt > test2_output.txt
-if diff -q test2_expected_output.txt test2_output.txt
+./shell_art < /tmp/test1_input.txt > test1_output.txt
+if diff -q /tmp/test1_expected_output.txt test1_output.txt
 then
     echo "Test 2 passed"
 else
@@ -19,8 +19,8 @@ else
 fi
 
 echo "Running test 3..."
-./shell_art < test3_input.txt > test3_output.txt
-if diff -q test3_expected_output.txt test3_output.txt
+./shell_art < /tmp/test2_input.txt > test2_output.txt
+if diff -q /tmp/test2_expected_output.txt test2_output.txt
 then
     echo "Test 3 passed"
 else
@@ -28,8 +28,8 @@ else
 fi
 
 echo "Running test 4..."
-./shell_art < test4_input.txt > test4_output.txt
-if diff -q test4_expected_output.txt test4_output.txt
+./shell_art < /tmp/test3_input.txt > test3_output.txt
+if diff -q /tmp/test3_expected_output.txt test3_output.txt
 then
     echo "Test 4 passed"
 else
@@ -37,8 +37,8 @@ else
 fi
 
 echo "Running test 5..."
-./shell_art < test5_input.txt > test5_output.txt
-if diff -q test5_expected_output.txt test5_output.txt
+./shell_art < /tmp/test4_input.txt > test54_output.txt
+if diff -q /tmp/test4_expected_output.txt test4_output.txt
 then
     echo "Test 5 passed"
 else
