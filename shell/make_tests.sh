f0747c7df307142d2518af0c8125b25f191412b2
tARTar78 <143780971+tARTar78@users.noreply.github.com>
2023-12-19 23:12:16 +0300

    Update make_tests.sh

--- shell/make_tests.sh
+++ shell/make_tests.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-commands=(
-  "echo 'Hello, world!'"
-  "ls -l"
-  "date"
-  "whoami"
-  "uname -a"
-)
+# Массив команд
+commands=("pwd" "echo Hello, World1231!" "yes | head" "df -h" "uname -a")
 
-for ((i=1; i<=${#commands[@]}; i++)); do
-  output=$(eval ${commands[$i]})
-  echo "${commands[$i]}; exit" > "test${i}_input.txt"
-  echo "$output" > "test${i}_expected_output.txt"
+# Создание временной директории
+#temp_dir=$(mktemp -d /tmp/command_output_XXXXX)
+
+# Переменная для индексации
+index=0
+
+# Перебор команд в массиве
+for command in "${commands[@]}"; do
+  # Имя файла для записи вывода
+  output_file="/tmp/test${index}_expected_output.txt"
+  # Имя файла для записи команды
+  command_file="/tmp/test${index}_input.txt"
+
+  # Запись команды в файл
+  echo "${command} ; exit" > "$command_file"
+
+  # Запуск команды, запись вывода в файл
+  eval $command > "$output_file"
+  #echo "\$art\$ " > "$output_file"
+ 	#eval $command | echo
+  #echo "\$art\$ $p" > "$output_file"
+  
+  # Увеличение индекса
+  ((index++))
 done
