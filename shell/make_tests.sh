2349ac482c1f86b9870330214aed9100fe298743
tARTar78 <143780971+tARTar78@users.noreply.github.com>
2023-12-19 19:51:57 +0300

    Update make_tests.sh

--- shell/make_tests.sh
+++ shell/make_tests.sh
@@ -10,6 +10,6 @@ commands=(
 
 for ((i=1; i<=${#commands[@]}; i++)); do
   output=$(eval ${commands[$i]})
-  echo "${commands[$i]}; exit" > "input${i}.txt"
+  echo "${commands[$i]}; exit" > "test${i}_input.txt.txt"
   echo "$output" > "test${i}_expected_output.txt"
 done
